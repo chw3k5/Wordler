733eaa8545c3fdeb1d05ca29363aade50eea5ba7
chw3k5 <chw3k5@gmail.com>
2022-02-18 18:10:29 -0600

    Updated gameplay to handle the display of double/multiple letters in the word you are trying to guess.

--- wordler/play.py
+++ wordler/play.py
@@ -59,6 +59,10 @@ class Wordle:
         self.known_in_right_place = None
         self.known_in_wrong_place = None
         self.known_wrong = None
+        self.letter_counter = None
+        self.word = None
+
+        self.games_number = 0
 
     def reset(self):
         self.number_of_guesses = 0
@@ -69,6 +73,9 @@ class Wordle:
         self.known_in_right_place = set()
         self.known_in_wrong_place = set()
         self.known_wrong = set()
+        self.letter_counter = {}
+
+        self.games_number += 1
 
     def get_word(self, word=None):
         if word is None:
@@ -77,7 +84,11 @@ class Wordle:
             self.available_words.remove(word)
         else:
             raise KeyError(f'The word {word} is not an allowed word.')
-        return word
+        self.word = word
+        for letter in self.word:
+            if letter not in self.letter_counter.keys():
+                self.letter_counter[letter] = 0
+            self.letter_counter[letter] += 1
 
     def ask_for_word(self):
         guess_word = None
@@ -95,33 +106,26 @@ class Wordle:
             self.unknown.remove(letter)
 
     def is_correct_place_letter(self, letter):
-        self.remove_unknown(letter=letter)
-        if letter in self.known_in_wrong_place:
-            self.known_in_wrong_place.remove(letter)
-        self.known_in_right_place.add(letter)
         self.share_text += is_correct_place_letter_text(letter=' ')
         return is_correct_place_letter_text(letter=letter)
 
     def is_used_but_place_is_wrong(self, letter):
-        self.remove_unknown(letter=letter)
-        self.known_in_wrong_place.add(letter)
         self.share_text += is_used_but_place_is_wrong_text(letter=' ')
         return is_used_but_place_is_wrong_text(letter=letter)
 
     def is_not_used_letter(self, letter):
-        self.remove_unknown(letter=letter)
-        self.known_wrong.add(letter)
         self.share_text += is_not_used_letter_text(letter=' ')
         return is_not_used_letter_text(letter=letter)
 
-    def make_letter_text(self, word, guess_letter, guess_index):
-        if guess_letter in word:
-            if guess_letter == word[guess_index]:
-                return self.is_correct_place_letter(letter=guess_letter)
-            else:
-                return self.is_used_but_place_is_wrong(letter=guess_letter)
-        else:
+    def make_letter_text(self, guess_letter, display_type):
+        if display_type == '2':
+            return self.is_correct_place_letter(letter=guess_letter)
+        elif display_type == '1':
+            return self.is_used_but_place_is_wrong(letter=guess_letter)
+        elif display_type == '0':
             return self.is_not_used_letter(letter=guess_letter)
+        else:
+            raise KeyError
 
     def get_console_text(self):
         if self.qwerty_console:
@@ -145,10 +149,51 @@ class Wordle:
             console_str += '\n'
         return console_str
 
-    def print_display(self, word, guess_word):
+    def determine_test_types(self, guess_word):
+        index_to_guess_letter_and_display_type = {}
+        remaining_word_indexes = list(range(len(guess_word)))
+        remaining_word_letters = list(self.word)
+        for correct_index in list(remaining_word_indexes):
+            guess_letter = guess_word[correct_index]
+            word_letter = self.word[correct_index]
+            if word_letter == guess_letter:
+                index_to_guess_letter_and_display_type[correct_index] = (guess_letter, '2')
+                self.remove_unknown(letter=guess_letter)
+                if guess_letter in self.known_in_wrong_place:
+                    self.known_in_wrong_place.remove(guess_letter)
+                self.known_in_right_place.add(guess_letter)
+                remaining_word_indexes.remove(correct_index)
+                remaining_word_letters.remove(guess_letter)
+
+        for is_used_index in list(remaining_word_indexes):
+            guess_letter = guess_word[is_used_index]
+            if guess_letter in remaining_word_letters:
+                index_to_guess_letter_and_display_type[is_used_index] = (guess_letter, '1')
+                self.remove_unknown(letter=guess_letter)
+                if guess_letter not in self.known_in_right_place:
+                    # this is not a double letter that is unused, but a letter that is in the word in different place
+                    self.known_in_wrong_place.add(guess_letter)
+                remaining_word_indexes.remove(is_used_index)
+                remaining_word_letters.remove(guess_letter)
+
+        for is_used_index in list(remaining_word_indexes):
+            guess_letter = guess_word[is_used_index]
+            index_to_guess_letter_and_display_type[is_used_index] = (guess_letter, '0')
+            self.remove_unknown(letter=guess_letter)
+            if guess_letter not in self.known_in_right_place and guess_letter not in self.known_in_wrong_place:
+                # this is not a double letter that is unused, but a letter the is not used at all
+                self.known_wrong.add(guess_letter)
+            remaining_word_indexes.remove(is_used_index)
+        if len(remaining_word_indexes) != 0:
+            raise TypeError
+        return index_to_guess_letter_and_display_type
+
+    def print_display(self, guess_word):
         text_str = ''
-        for guess_index, guess_letter in list(enumerate(guess_word)):
-            text_str += self.make_letter_text(word=word, guess_letter=guess_letter, guess_index=guess_index)
+        index_to_guess_letter_and_display_type = self.determine_test_types(guess_word)
+        for guess_index in sorted(index_to_guess_letter_and_display_type.keys()):
+            guess_letter, display_type = index_to_guess_letter_and_display_type[guess_index]
+            text_str += self.make_letter_text(guess_letter=guess_letter, display_type=display_type)
         console_str = self.get_console_text()
         self.display_history += text_str + '\n'
         self.share_text += '\n'
@@ -159,11 +204,11 @@ class Wordle:
 
     def play(self, word=None):
         self.reset()
-        word = self.get_word(word=word)
+        self.get_word(word=word)
         guess_word = None
-        while guess_word != word:
+        while guess_word != self.word:
             guess_word = self.ask_for_word()
-            self.print_display(word=word, guess_word=guess_word)
+            self.print_display(guess_word=guess_word)
 
         punctuation = get_punctuation(number_of_guesses=self.number_of_guesses)
         print(f'\n\nCompleted in {self.number_of_guesses} guesses{punctuation}\n')
@@ -176,6 +221,10 @@ def play(qwerty_console=True):
     play_again = True
     while play_again:
         w.play()
+        ess = 's'
+        if w.games_number < 2:
+            ess = ''
+        print(f'{w.games_number} game{ess} played, {w.available_words} words remain.')
         raw_play_again = input('\nDo you want to play again with a new word? [y,n]:\n ')
         if raw_play_again.strip().lower() in allowed_true:
             play_again = True
